040cf3d9462049e07b00495cae8a7de7ed0b7483
keerthan <krishnasmile2020@gmail.com>
2016-03-13 20:45:36 +0530

    Login activity -keerthan

---
+++ app/src/main/java/aves/deliveryapp/activity/MainActivity.java
@@ -0,0 +1,20 @@
+package aves.deliveryapp.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import aves.deliveryapp.R;
+
+/**
+ * Created on 3/13/2016.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
